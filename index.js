b7f757e3eb8d6f373714c070e53822e7a2724939
Priscilla Reis <priscillareissouza@outlook.com>
2020-07-15 22:01:29 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import { studentRouter } from './routes/studentsRouter.js';
+
+const app = express();
+
+const connection = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://dbUser:dbUserPassword@cluster0.drx3n.mongodb.net/grades?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+  } catch (error) {
+    console.log('Error to connect');
+  }
+};
+
+connection();
+
+app.use(express.json());
+
+app.use(studentRouter);
+
+app.listen(3001, () => {
+  console.log('API Iniciada');
+});
---
+++ routes/studentsRouter.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import { studentModel } from '../models/student.js';
+
+const app = express();
+
+app.get('/student', async (req, res) => {
+  try {
+    const student = await studentModel.find();
+    res.send(student);
+  } catch (error) {
+    res.status(500).send('Error to connect');
+  }
+});
+
+app.post('/student', async (req, res) => {
+  try {
+    const student = new studentModel(req.body);
+    await student.save();
+    res.send(student);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.delete('/student/:id', async (req, res) => {
+  try {
+    const student = await studentModel.findOneAndDelete({ _id: req.params.id });
+    if (!student) {
+      res.status(404).send('Documento não encontrado');
+    }
+    res.send('Deleted');
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.patch('/student/:id', async (req, res) => {
+  try {
+    const student = await studentModel.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true });
+    res.send(student);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+export { app as studentRouter };
